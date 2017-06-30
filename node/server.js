372d879df9d89047b339b4e41b909f6760d6ab43
jmzjmzjmzjmz <1454242069@qq.com>
2017-06-30 18:43:39 +0800

    2017/6/30

--- node/server.js
+++ node/server.js
@@ -15,23 +15,26 @@ http.createServer( function (request, response) {
     console.log("Request for " + pathname + " received.");
 
     // 从文件系统中读取请求的文件内容
-    fs.readFile(pathname.substr(1), function (err, data) {
-        if (err) {
-            console.log(err);
-            // HTTP 状态码: 404 : NOT FOUND
-            // Content Type: text/plain
-            response.writeHead(404, {'Content-Type': 'text/html'});
-        }else{
-            // HTTP 状态码: 200 : OK
-            // Content Type: text/plain
-            response.writeHead(200, {'Content-Type': 'text/html'});
-
-            // 响应文件内容
-            response.write(data.toString());
-        }
-        //  发送响应数据
-        response.end();
-    });
+    // fs.readFile(pathname.substr(1), function (err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         // HTTP 状态码: 404 : NOT FOUND
+    //         // Content Type: text/plain
+    //         response.writeHead(404, {'Content-Type': 'text/html'});
+    //     }else{
+    //         // HTTP 状态码: 200 : OK
+    //         // Content Type: text/plain
+    //         response.writeHead(200, {'Content-Type': 'text/html'});
+    //
+    //         // 响应文件内容
+    //         response.write(data.toString());
+    //     }
+    //     //  发送响应数据
+    //     response.end();
+    // });
+    response.write("<head><meta charset='utf-8'/></head>");
+    response.write("哈哈哈哈");
+    response.end();
 }).listen(8081);
 
 // 控制台会输出以下信息
